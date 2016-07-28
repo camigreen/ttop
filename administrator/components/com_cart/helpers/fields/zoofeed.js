ffd95d372819f5d4045ffea8bd1d4403ec93f3a2
Shawn Gibbons <sgibbons@palmettoimages.com>
2016-07-28 01:14:26 +0000

    removed tracked files

--- administrator/components/com_cart/helpers/fields/zoofeed.js
+++
@@ -1,3 +0,0 @@
-/* Copyright (C) YOOtheme GmbH, http://www.gnu.org/licenses/gpl.html GNU/GPL */
-
-jQuery(function($){$("div.zoo-feed").each(function(){var input=$(this).find("div.input");var radios=$(this).find("input:radio");if(radios.first().is(":checked"))input.hide();radios.bind("change",function(){input.slideToggle()})})});
--- administrator/components/com_jmap/sql/uninstall.mysql.utf8.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS `#__jmap`;
-DROP TABLE IF EXISTS `#__jmap_pingomatic`;
-DROP TABLE IF EXISTS `#__jmap_menu_priorities`;
