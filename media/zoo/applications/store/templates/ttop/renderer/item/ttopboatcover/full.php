754f666767915b11e1237c7adeca20a1d61ebe67
camigreen <sgibbons@palmettoimages.com>
2016-09-01 12:04:55 -0400

    Slideshow hack

--- media/zoo/applications/store/templates/ttop/renderer/item/ttopboatcover/full.php
+++ media/zoo/applications/store/templates/ttop/renderer/item/ttopboatcover/full.php
@@ -314,7 +314,7 @@ jQuery(function($){
                         onInit: [
                             function (data) {
                                 console.log(data);
-                                $("#eb48f28a-10ec-43be-b647-d850d19e35f1-57c849325caee").slideshow('resize');
+                                $("div.zoo-gallery").slideshow('resize');
                                 var item;
                                 $.each(data.args.items, function (k, v) {
                                     if(this.type == 'ttopboatcover') {
