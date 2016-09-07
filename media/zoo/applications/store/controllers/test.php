42e1f52d991b7ac1103e242a0d9c85805b070795
camigreen <sgibbons@palmettoimages.com>
2016-09-07 11:45:23 -0400

    update test.php

--- media/zoo/applications/store/controllers/test.php
+++ media/zoo/applications/store/controllers/test.php
@@ -179,30 +179,41 @@ class TestController extends AppController {
 	}
 
 	public function PriceList() {
+		$model = $this->app->request->get('m', 'string', null);
 		$fabrics = array('7oz', '8oz', '9oz');
 		$options = array('conditions' => array("type = 'ttopboatcover' AND state = 1"));
 		$_items = $this->app->table->item->all($options);
 		$items = $this->app->data->create();
+		$product = array();
 		foreach($_items as $item) {
 			$cat = $item->getPrimaryCategory()->name;
-			foreach($fabrics as $fabric) {
-				$product = $this->app->parameter->create();
+			if($cat == $model) {
 				$item = $this->app->item->create($item, 'ttopboatcover');
-				$item->options['fabric']->set('value', $fabric);
-				$price = $item->getPrice();
-				$product[$item->attributes['boat_model']->get('text')] = array();
-				$product[$item->attributes['boat_model']->get('text')][$fabric] = $price->get();
+				
+				if(!isset($product[$item->attributes['boat_model']->get('text')])) {
+					$product[$item->attributes['boat_model']->get('text')] = array();
+				}
+				foreach($fabrics as $fabric) {
+					$item->options['fabric']->set('value', $fabric);
+					$price = $item->getPrice();
+					$product[$item->attributes['boat_model']->get('text')][$fabric] = $this->app->number->currency($price->get(),array('currency' => 'USD'));
+				}
+				
+				$items->set($cat, $product);
 			}
-			
-			$items->set($cat, $product);
 		}
+		$items = (array) $items;
 		ksort($items);
-		// var_dump($items);
-		// return;
-		foreach($items as $cat => $item) {
+		//var_dump($items['Albemarle']);
+		//return;
+		foreach($items as $cat => $models) {
 			echo '<h3 class="uk-text-contrast">'.$cat.'</h3></br>';
-			foreach($item as $fabric) {
-				// list($)
+			foreach($models as $model => $prices) {
+					echo "<h4 class='uk-text-contrast'>$model</h4>";
+				foreach($prices as $fabric => $price) {
+					echo "<p>$fabric  -  $price</p>";
+				}
+				
 			}
 		}
 	}
