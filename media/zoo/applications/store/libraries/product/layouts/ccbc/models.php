c3524ec199994c9ee212484a0763174449ed7bb9
camigreen <sgibbons@palmettoimages.com>
2016-12-05 14:58:47 -0500

    updates

--- media/zoo/applications/store/libraries/product/layouts/ccbc/models.php
+++ media/zoo/applications/store/libraries/product/layouts/ccbc/models.php
@@ -24,12 +24,14 @@ $models = $this->manufacturer->get('models');
 		<span class="uk-h3">Available Models</span>
 	</div>
 	<?php foreach($models as $model) : ?>
-	<div class="uk-width-medium-1-3">
-		<div class="uk-panel uk-panel-box">
-			<div class="uk-h4">
-				<a href="<?php echo $this->url.$this->manufacturer->name.'/'.$model->name; ?>"><?php echo $model->label; ?></a>
+	<a href="<?php echo $this->url.$this->manufacturer->name.'/'.$model->name; ?>">
+		<div class="uk-width-medium-1-3">
+			<div class="uk-panel uk-panel-box">
+				<div class="uk-h4">
+					<?php echo $model->label; ?>
+				</div>
 			</div>
 		</div>
-	</div>
+	</a>
 	<?php endforeach; ?>
 </div>
