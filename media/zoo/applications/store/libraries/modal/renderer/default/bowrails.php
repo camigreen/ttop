71161aad702477ea5c276ac33029bad975f491f3
camigreen <sgibbons@palmettoimages.com>
2016-12-05 14:20:05 -0500

    fixed bow rails modal

--- media/zoo/applications/store/libraries/modal/renderer/default/bowrails.php
+++ media/zoo/applications/store/libraries/modal/renderer/default/bowrails.php
@@ -1,77 +1,75 @@
-<div class="uk-grid" data-uk-grid-margin>
-	<ul class="uk-grid" data-uk-grid-margin>
-		<li class="uk-width-medium-1-2">
-			<div class="uk-thumbnail">
-				<div class="uk-thumbnail-caption">
-					<div>
-						<label><input name="bowrails_modal_helper" type="radio" value="N" /> No Bow Rail</label>
-					</div>
-					<div>
-						<span>(Pictured Below)</span>
-					</div>
+<ul class="uk-grid" data-uk-grid-margin>
+	<li class="uk-width-medium-1-2">
+		<div class="uk-thumbnail">
+			<div class="uk-thumbnail-caption">
+				<div>
+					<label><input name="bowrails_modal_helper" type="radio" value="N" /> No Bow Rail</label>
 				</div>
-				<img src="/images/helpers/bowaccessories/CleanerBow.png" alt="CleanerBow" vspace="10" />
-			</div>
-		</li>
-		<li class="uk-width-medium-1-2">
-			<div class="uk-thumbnail">
-				<div class="uk-thumbnail-caption">
-					<div>
-						<label><input name="bowrails_modal_helper" type="radio" value="L" /> Low Bow Rail (Less than 8")</label>
-					</div>
-					<div>
-						(Pictured Below)
-					</div>
+				<div>
+					<span>(Pictured Below)</span>
 				</div>
-					<img src="/images/helpers/LowBR.png" alt="LowBR" vspace="10" />
 			</div>
-		</li>
-		<li class="uk-width-medium-1-2">
-			<div class="uk-thumbnail">
-				<div class="uk-thumbnail-caption">
-					<div>
-						<label><input name="bowrails_modal_helper" type="radio" value="R" /> Reccessed Bow Rail</label>
-					</div>
-					<div>
-						<span>(Pictured Below)</span>
-					</div>
+			<img src="/images/helpers/bowaccessories/CleanerBow.png" alt="CleanerBow" vspace="10" />
+		</div>
+	</li>
+	<li class="uk-width-medium-1-2">
+		<div class="uk-thumbnail">
+			<div class="uk-thumbnail-caption">
+				<div>
+					<label><input name="bowrails_modal_helper" type="radio" value="L" /> Low Bow Rail (Less than 8")</label>
 				</div>
-				<img src="/images/helpers/RecessedBowRail.png" alt="RecessedBowRail" vspace="10" />
-			</div>
-		</li>
-		<li class="uk-width-medium-1-2">
-			<div class="uk-thumbnail">
-				<div class="uk-thumbnail-caption">
-					<div>
-						<label><input name="bowrails_modal_helper" type="radio" value="H" /> High Bow Rail (More than 9")</label>
-					</div>
-					<div>
-						<span>(Pictured Below)</span>
-					</div>
+				<div>
+					(Pictured Below)
 				</div>
-				<img src="/images/helpers/HighBR.png" alt="HighBR" vspace="10" />
 			</div>
-		</li>
-		<li class="uk-width-medium-1-2">
-			<div class="uk-thumbnail">
-				<div class="uk-thumbnail-caption">
-					<div>
-						<label><input name="bowrails_modal_helper" type="radio" value="WT" /> Walkthrough Bow Rail</label>
-					</div>
-					<div>
-						<span style="font-size: 10pt;">(Pictured Below)</span>
-					</div>
+				<img src="/images/helpers/LowBR.png" alt="LowBR" vspace="10" />
+		</div>
+	</li>
+	<li class="uk-width-medium-1-2">
+		<div class="uk-thumbnail">
+			<div class="uk-thumbnail-caption">
+				<div>
+					<label><input name="bowrails_modal_helper" type="radio" value="R" /> Reccessed Bow Rail</label>
 				</div>
-				<img src="/images/helpers/WalkThrough.png" alt="WalkThrough" vspace="10" />
+				<div>
+					<span>(Pictured Below)</span>
+				</div>
 			</div>
-		</li>
-	</ul>
-	<div class="uk-width-1-1">
-		<label>
-			<input name="bowrails_modal_helper" type="radio" value="other" />
-			Other<span class="uk-text-danger">(Please leave a description of your bow rails in the additional infomation box on the order page)</span>
-		</label>
-	</div>
+			<img src="/images/helpers/RecessedBowRail.png" alt="RecessedBowRail" vspace="10" />
+		</div>
+	</li>
+	<li class="uk-width-medium-1-2">
+		<div class="uk-thumbnail">
+			<div class="uk-thumbnail-caption">
+				<div>
+					<label><input name="bowrails_modal_helper" type="radio" value="H" /> High Bow Rail (More than 9")</label>
+				</div>
+				<div>
+					<span>(Pictured Below)</span>
+				</div>
+			</div>
+			<img src="/images/helpers/HighBR.png" alt="HighBR" vspace="10" />
+		</div>
+	</li>
+	<li class="uk-width-medium-1-2">
+		<div class="uk-thumbnail">
+			<div class="uk-thumbnail-caption">
+				<div>
+					<label><input name="bowrails_modal_helper" type="radio" value="WT" /> Walkthrough Bow Rail</label>
+				</div>
+				<div>
+					<span style="font-size: 10pt;">(Pictured Below)</span>
+				</div>
+			</div>
+			<img src="/images/helpers/WalkThrough.png" alt="WalkThrough" vspace="10" />
+		</div>
+	</li>
+</ul>
+<div class="uk-width-1-1">
+	<label>
+		<input name="bowrails_modal_helper" type="radio" value="other" />
+		Other<span class="uk-text-danger">(Please leave a description of your bow rails in the additional infomation box on the order page)</span>
+	</label>
 </div>
 <script>
 jQuery(function($){
