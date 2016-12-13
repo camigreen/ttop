612edf49247f08923be57f418918914794f06952
camigreen <sgibbons@palmettoimages.com>
2016-12-13 07:57:14 -0500

    removed var dump from ttop template

--- media/zoo/applications/store/templates/ttop/renderer/item/ttopboatcover/full.php
+++ media/zoo/applications/store/templates/ttop/renderer/item/ttopboatcover/full.php
@@ -15,7 +15,7 @@ $this->app->document->addScript('library.cart:assets/js/cart.js');
 $this->app->document->addScript('library.product:assets/js/orderform.js');
 $this->app->document->addScript('assets:/jquery-ui-1.12.1/jquery-ui.min.js');
 $this->app->document->addStyleSheet('assets:/jquery-ui-1.12.1/jquery-ui.min.css');
-$product->price->debug(true);
+// $product->price->debug(true);
 
 ?>
 <div id="OrderForm-<?php echo $product->id; ?>" class="t-top-boat-cover" data-id="<?php echo $product->id; ?>">
